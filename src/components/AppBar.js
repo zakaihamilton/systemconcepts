136832c762a447aac952d19b33754f1028f8a07a
Zakai Hamilton <zakaihamilton@gmail.com>
2020-07-28 11:25:29 +0300

    App Icon is now Menu Icon

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./AppBar/AppBar.module.scss"
-import AppIcon from "./AppBar/AppIcon";
+import MenuIcon from "./AppBar/MenuIcon";
 import AppTitle from "./AppBar/AppTitle";
 
 export default function AppBar() {
     return <header className={styles.root}>
-        <AppIcon />
+        <MenuIcon />
         <AppTitle />
     </header>;
 }
--- src/components/AppBar/MenuIcon.js
+++ src/components/AppBar/MenuIcon.js
@@ -1,16 +1,23 @@
 import React from "react";
 import IconButton from "@/components/Widgets/IconButton";
-import AppsIcon from '@material-ui/icons/Apps';
+import MenuIcon from '@material-ui/icons/Menu';
 import Tooltip from '@material-ui/core/Tooltip';
 import { nextTrimmedString } from "@/util/array";
 import { MainStore } from "../Main";
 
-export default function AppIcon() {
+export default function Menu() {
 
     const toggleMenu = () => {
         MainStore.update(s => {
             if (s.showSideBar) {
-                s.menuViewList = nextTrimmedString(["List", "IconList"], s.menuViewList);
+                const menuViewList = nextTrimmedString(["None", "List", "IconList"], s.menuViewList);
+                if (menuViewList === "None") {
+                    s.menuViewList = "List";
+                    s.showSideBar = false;
+                }
+                else {
+                    s.menuViewList = menuViewList;
+                }
             }
             else {
                 s.showSideBar = true;
@@ -19,8 +26,8 @@ export default function AppIcon() {
     };
 
     return <IconButton onClick={toggleMenu}>
-        <Tooltip arrow title="Apps">
-            <AppsIcon />
+        <Tooltip arrow title="Menu">
+            <MenuIcon />
         </Tooltip>
     </IconButton>;
 }
