7f173671abb0b2b6591ff3c3f997258af3b3b620
Zakai Hamilton <zakaihamilton@gmail.com>
2020-07-29 17:02:18 +0300

    loading style fixes

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,9 @@ import styles from "./Loading.module.scss";
 export default function Loading() {
     return (
         <div className={styles.root}>
-            <LinearProgress />
+            <div className={styles.progress}>
+                <LinearProgress />
+            </div>
         </div>
     );
 }
