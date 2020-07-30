bb65aa470ff5d702a41610895fbb5ec553195fd4
Zakai Hamilton <zakaihamilton@gmail.com>
2020-07-30 14:10:21 +0300

    use input on mobile if more than 3 items

--- src/components/Widgets/Dynamic.js
+++ src/components/Widgets/Dynamic.js
@@ -1,9 +1,11 @@
 import ToggleButtonGroup from "./ToggleButtonGroup";
 import Input from "./Input";
+import { useImportMedia } from "@/util/styles";
 
 export default function DynamicWidget({ state, items, ...props }) {
+    const isMobile = useImportMedia(im => im.lessThan('tablet'));
     let Component = ToggleButtonGroup;
-    if (items.length > 3) {
+    if (items.length > 8 || (isMobile && items.length > 3)) {
         Component = Input;
         props.select = true;
     }
