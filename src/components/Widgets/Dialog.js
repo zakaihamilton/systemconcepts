e7b9d47b2d1e2cecb6778c72cb9e24c1906c0958
Zakai Hamilton <zakaihamilton@gmail.com>
2020-07-31 17:37:58 +0300

    bug fixes

--- src/components/Widgets/Dialog.js
+++ src/components/Widgets/Dialog.js
@@ -9,23 +9,25 @@ export default function Dialog({ title, children, actions, onClose }) {
     const translations = useTranslations();
 
     return <div className={styles.root}>
-        <div className={styles.dialog}>
-            <div className={styles.title}>
-                <Typography variant="h6">
-                    {title}
-                </Typography>
-                <div style={{ flex: 1 }} />
-                <IconButton className={styles.closeDialog} onClick={onClose}>
-                    <Tooltip title={translations.CLOSE} arrow>
-                        <CancelIcon />
-                    </Tooltip>
-                </IconButton>
-            </div>
-            <div className={styles.content}>
-                {children}
-            </div>
-            <div className={styles.actions}>
-                {actions}
+        <div className={styles.background}>
+            <div className={styles.dialog}>
+                <div className={styles.title}>
+                    <Typography variant="h6">
+                        {title}
+                    </Typography>
+                    <div style={{ flex: 1 }} />
+                    <IconButton className={styles.closeDialog} onClick={onClose}>
+                        <Tooltip title={translations.CLOSE} arrow>
+                            <CancelIcon />
+                        </Tooltip>
+                    </IconButton>
+                </div>
+                <div className={styles.content}>
+                    {children}
+                </div>
+                <div className={styles.actions}>
+                    {actions}
+                </div>
             </div>
         </div>
     </div>
