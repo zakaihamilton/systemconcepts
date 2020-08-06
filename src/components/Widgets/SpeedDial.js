e783184988d1e2b26d9bb842a84e756fe9b5c720
Zakai Hamilton <zakaihamilton@gmail.com>
2020-08-06 21:16:07 +0300

    speed dial supports right to left

--- src/components/Widgets/SpeedDial.js
+++ src/components/Widgets/SpeedDial.js
@@ -1,39 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Backdrop from '@material-ui/core/Backdrop';
 import SpeedDial from '@material-ui/lab/SpeedDial';
 import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
 import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        height: 380,
-        transform: 'translateZ(0px)',
-        flexGrow: 1,
-    },
-    speedDial: {
-        position: 'absolute',
-        bottom: "2em",
-        right: "2em"
-    },
-    tooltip: {
-        width: "10em"
-    },
-    fab: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "3em",
-        height: "3em"
-    },
-    icon: {
-        width: "3.5em",
-        height: "3.5em"
-    }
-}));
+import { MainStore } from "@/components/Main";
+import styles from "./SpeedDial.module.scss";
 
 export default function SpeedDialWidget({ visible = true, items }) {
-    const classes = useStyles();
+    const { direction } = MainStore.useState();
     const [open, setOpen] = React.useState(false);
 
     const handleOpen = () => {
@@ -57,25 +31,26 @@ export default function SpeedDialWidget({ visible = true, items }) {
             icon={item.icon}
             tooltipTitle={item.name}
             tooltipOpen
+            tooltipPlacement={direction === "rtl" ? "right" : "left"}
             onClick={itemHandler}
             classes={{
-                fab: classes.icon,
-                staticTooltipLabel: classes.tooltip
+                fab: styles.icon,
+                staticTooltipLabel: styles.tooltip
             }}
         />;
     });
 
     return (
-        <div className={classes.root}>
+        <div className={styles.root}>
             <Backdrop open={open} />
             <SpeedDial
                 ariaLabel=""
                 classes={{
-                    root: classes.speedDial,
-                    fab: classes.fab
+                    root: direction === "rtl" ? styles.speedDialRtl : styles.speedDial,
+                    fab: styles.fab
                 }}
                 hidden={!visible}
-                icon={<SpeedDialIcon classes={{ root: classes.fab }} />}
+                icon={<SpeedDialIcon classes={{ root: styles.fab }} />}
                 onClose={handleClose}
                 onOpen={handleOpen}
                 open={open}
