a30966f7d887d106702a923e13aa218154a2725a
Zakai Hamilton <zakaihamilton@gmail.com>
2020-08-13 22:18:14 +0300

    no need for backdrop

--- src/components/Widgets/SpeedDial.js
+++ src/components/Widgets/SpeedDial.js
@@ -42,7 +42,6 @@ export default function SpeedDialWidget({ visible = true, items }) {
 
     return (
         <div className={styles.root}>
-            <Backdrop open={open} />
             <SpeedDial
                 ariaLabel=""
                 classes={{
