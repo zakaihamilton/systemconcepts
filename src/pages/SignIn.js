ba4cdb70cdbf6b2c14a777f4b025ad0066362998
Zakai Hamilton <zakaihamilton@gmail.com>
2020-08-23 11:20:55 +0300

    fixed spacing

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -19,7 +19,7 @@ import Input from "@/widgets/Input";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        marginTop: theme.spacing(4),
+        marginTop: theme.spacing(2),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -19,7 +19,7 @@ import Input from "@/widgets/Input";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        marginTop: theme.spacing(8),
+        marginTop: theme.spacing(2),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
