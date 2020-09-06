8093a8413dff0cb6e5e8f2cdb8e2d2cc83f420f9
Zakai Hamilton <zakaihamilton@gmail.com>
2020-09-06 13:31:13 +0300

    bug fix

--- src/components/Widgets/Table.js
+++ src/components/Widgets/Table.js
@@ -45,7 +45,7 @@ function stableSort(array, comparator) {
     return stabilizedThis.map((el) => el[0]);
 }
 
-export default function TableWidget({ name, rowHeight = "4em", columns, depends = [], reset, sortColumn, data, mapper, filter, empty, statusBar, className, hideColumns, rowClick, ...props }) {
+export default function TableWidget({ name, rowHeight = "4em", columns, depends = [], reset = [], sortColumn, data, mapper, filter, empty, statusBar, className, hideColumns, rowClick, ...props }) {
     const translations = useTranslations();
     const [order, setOrder] = React.useState("desc");
     const [offset, setOffset] = React.useState(0);
