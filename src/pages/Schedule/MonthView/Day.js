ec9a896729c947afe7d79ae8e0688c8fc49f5ad5
Zakai Hamilton <zakaihamilton@gmail.com>
2020-10-07 00:44:39 +0300

    sessions separated into media types

--- src/pages/Schedule/MonthView/Day.js
+++ src/pages/Schedule/MonthView/Day.js
@@ -21,32 +21,27 @@ export default function Day({ sessions, month, column, row, date, dateFormatter 
     const isToday = isDateToday(date);
     const isMonth = isDateMonth(date, month);
     const sessionDate = getDateString(date);
-    const sessionItems = sessions.filter(session => session.date === sessionDate).map(item => {
-        const media = [];
+    const sessionItems = sessions.filter(session => session.date === sessionDate);
+    const audioItems = sessionItems.filter(item => item.audio).map(item => {
         const groupName = item.group[0].toUpperCase() + item.group.slice(1);
-        if (item.audio) {
-            media.push({
-                id: "audio" + item.name,
-                name: item.name,
-                icon: <Tooltip title={translations.AUDIO}><AudiotrackIcon /></Tooltip>,
-                description: groupName,
-                onClick: () => addPath(`player?prefix=sessions&group=${item.group}&year=${item.year}&name=${item.date + " " + item.name}&suffix=.m4a`)
-            });
-        }
-        if (item.video) {
-            media.push({
-                id: "video" + item.name,
-                name: item.name,
-                icon: <Tooltip title={translations.VIDEO}><MovieIcon /></Tooltip>,
-                description: groupName,
-                onClick: () => addPath(`player?prefix=sessions&group=${item.group}&year=${item.year}&name=${item.date + " " + item.name}&suffix=.mp4`)
-            });
-        }
-        if (media.length) {
-            media[media.length - 1].divider = true;
-        }
-        return media;
-    }).flat();
+        return {
+            id: "audio" + item.name,
+            name: item.name,
+            icon: <Tooltip title={translations.PLAYER}><VideoLabelIcon /></Tooltip>,
+            description: groupName,
+            onClick: () => addPath(`player?prefix=sessions&group=${item.group}&year=${item.year}&name=${item.date + " " + item.name}&suffix=.m4a`)
+        };
+    });
+    const videoItems = sessionItems.filter(item => item.video).map(item => {
+        const groupName = item.group[0].toUpperCase() + item.group.slice(1);
+        return {
+            id: "video" + item.name,
+            name: item.name,
+            icon: <Tooltip title={translations.PLAYER}><VideoLabelIcon /></Tooltip>,
+            description: groupName,
+            onClick: () => addPath(`player?prefix=sessions&group=${item.group}&year=${item.year}&name=${item.date + " " + item.name}&suffix=.mp4`)
+        };
+    });
     return <div className={styles.root} style={style}>
         <div className={styles.title}>
             <Avatar className={clsx(styles.day, isToday && styles.today, isMonth && styles.active)}>
@@ -54,10 +49,17 @@ export default function Day({ sessions, month, column, row, date, dateFormatter 
             </Avatar>
         </div>
         <div className={styles.sessions}>
-            {sessionItems.length && <Menu items={sessionItems}>
+            {audioItems.length && <Menu items={audioItems}>
                 <IconButton>
-                    <Tooltip arrow title={translations.SESSIONS}>
-                        <VideoLabelIcon />
+                    <Tooltip arrow title={translations.AUDIO}>
+                        <AudiotrackIcon />
+                    </Tooltip>
+                </IconButton>
+            </Menu>}
+            {videoItems.length && <Menu items={videoItems}>
+                <IconButton>
+                    <Tooltip arrow title={translations.VIDEO}>
+                        <MovieIcon />
                     </Tooltip>
                 </IconButton>
             </Menu>}
