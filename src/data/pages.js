92bda01b58b4a6de6df6e0bc4b1b63f01928aa78
Zakai Hamilton <zakaihamilton@gmail.com>
2020-10-08 20:36:13 +0300

    moved account to quick access sidebar

--- src/data/pages.js
+++ src/data/pages.js
@@ -153,7 +153,9 @@ export default [
         id: "account",
         name: "ACCOUNT",
         icon: <AccountCircleIcon />,
-        Component: Account
+        Component: Account,
+        sidebar: true,
+        category: "quickaccess"
     },
     {
         id: "signup",
