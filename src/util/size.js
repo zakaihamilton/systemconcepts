66de6e96c9eda45c1e50a1fe2c01aa3fa345fe3d
Zakai Hamilton <zakaihamilton@gmail.com>
2020-10-22 09:48:02 +0300

    support for vw units

--- src/util/size.js
+++ src/util/size.js
@@ -18,6 +18,8 @@ export function useResize(ref, depends = []) {
         }
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
+        let vw = window.innerWidth * 0.01;
+        document.documentElement.style.setProperty('--vw', `${vw}px`);
         setTimeout(() => {
             if (!ref.current) {
                 return;
