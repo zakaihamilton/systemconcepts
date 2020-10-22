1a5eafa0c3f3dd9619939e691c1a2987d2c94b1b
Zakai Hamilton <zakaihamilton@gmail.com>
2020-10-22 14:59:01 +0300

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,6 @@ import Toolbar from "./Toolbar";
 
 export default function Header() {
     return <div className={styles.root}>
-        <Toolbar className={styles.toolbar} location="footer" />
+        <Toolbar location="footer" />
     </div>
 }
