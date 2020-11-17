44f2e2926697084ba43e603b2c45651a666ad7f2
Zakai Hamilton <zakaihamilton@gmail.com>
2020-11-17 19:16:48 +0200

    menu order fix

--- src/components/Widgets/Table.js
+++ src/components/Widgets/Table.js
@@ -34,7 +34,7 @@ import { StatusBarStore } from "@widgets/StatusBar";
 
 const collator = new Intl.Collator("en", { numeric: true, sensitivity: "base" });
 
-registerToolbar("Table", 200);
+registerToolbar("Table", 100);
 
 function descendingComparator(a, b, orderBy) {
     const aText = a && a[orderBy] || "";
