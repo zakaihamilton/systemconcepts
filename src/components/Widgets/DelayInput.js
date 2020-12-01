92bc1a924167de06a6c089f80f60696986367d30
Zakai Hamilton <zakaihamilton@gmail.com>
2020-12-01 17:12:25 +0200

    delayed input support in navigator

---
+++ src/components/Widgets/DelayInput.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { useTimeout } from "@util/timers";
+
+export default React.forwardRef(function DelayInput({ children, onChange, value, delay = 250 }, ref) {
+    const [currentValue, setCurrentValue] = useState(value);
+    useEffect(() => {
+        setCurrentValue(value);
+    }, [value]);
+    useTimeout(() => {
+        onChange && onChange({ target: { value: currentValue } });
+    }, delay, [currentValue]);
+    const onCurrentChange = event => {
+        const value = event.target.value;
+        setCurrentValue(value);
+    };
+
+    return React.Children.map(children, child => {
+        return React.cloneElement(child, { ref, onChange: onCurrentChange, value: currentValue });
+    });
+});
