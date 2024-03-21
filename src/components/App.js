1e11ed67f315bba754c5afc4eeb25cb08283d10f
Zakai Hamilton <zakaihamilton@gmail.com>
2024-03-21 11:28:39 +0200

    analytics

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import Theme from "./Theme";
 import Head from "./Head";
 import Main from "./Main";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
     return <React.StrictMode>
         <SpeedInsights />
+        <Analytics />
         <Head />
         <Theme>
             <Main />
