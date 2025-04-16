66680b5d715f666558aec12c9952eed1b7a5cbfb
zakaihamilton <zakaihamilton@gmail.com>
2025-04-16 11:22:59 +0300

    Upgrade packages (#124)
    * upgrade packages
    
    * lint fixes
    
    * added clsx
    
    * direction fix
    
    * lint fix
    
    * faster speed

--- src/pages/Player/Toolbar.js
+++ src/pages/Player/Toolbar.js
@@ -35,7 +35,8 @@ export default function Toolbar({ show, playerRef, isVideo }) {
         SPEED_NORMAL: 1.0,
         SPEED_FASTER: 1.1,
         SPEED_FAST: 1.25,
-        SPEED_SUPER_FAST: 1.5,
+        SPEED_VERY_FAST: 1.5,
+        SPEED_SUPER_FAST: 1.75,
         SPEED_DOUBLE: 2.0,
         SPEED_TRIPLE: 3.0
     };
