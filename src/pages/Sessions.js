508f5b88b420e2d6fc6ef2cfa1249ffc82d64f96
zakaihamilton <zakaihamilton@gmail.com>
2025-12-09 00:07:17 +0200

    fix: Add item type ID to icon click handler for better event handling

--- src/pages/Sessions.js
+++ src/pages/Sessions.js
@@ -169,7 +169,7 @@ export default function SessionsPage() {
             });
         };
         const icon = <Tooltip arrow title={title}>
-            <div style={style} className={styles.icon} onClick={onClickIcon}>
+            <div style={style} className={styles.icon} onClick={onClickIcon} id={item.type}>
                 {!!item.video && <MovieIcon />}
                 {!!item.audio && !item.video && <AudioIcon />}
                 {!!item.thumbnail && !item.video && !item.audio && <ImageIcon />}
