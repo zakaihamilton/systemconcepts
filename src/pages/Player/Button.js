b2c35bc0cde83c798fadfaf1a9240d65723c4cfc
zakaihamilton <zakaihamilton@gmail.com>
2025-12-25 23:24:02 +0200

    feat: Filter `active` prop from Button component's DOM attributes and add `align-items: flex-start` to session layout.

--- src/pages/Player/Button.js
+++ src/pages/Player/Button.js
@@ -2,7 +2,10 @@ import styles from "./Button.module.scss";
 import clsx from "clsx";
 
 export default function Button({ id, name, icon, active, subHeading, onClick, ...props }) {
-    return <button className={clsx(styles.root, active && styles.active)} onClick={onClick} {...props}>
+    // Remove active from props to prevent it from being passed to DOM
+    const { active: _, ...buttonProps } = { active, ...props };
+
+    return <button className={clsx(styles.root, active && styles.active)} onClick={onClick} {...buttonProps}>
         <div className={styles.icon}>
             {icon}
         </div>
