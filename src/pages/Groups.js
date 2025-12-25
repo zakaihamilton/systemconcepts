c3da29718c99ae83ea72b11a3dd63eab36b618b2
zakaihamilton <zakaihamilton@gmail.com>
2025-12-25 18:44:16 +0200

    feat: Replace custom color picker popover with MUI Dialog, enhancing styling and adding a title.

--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -135,7 +135,7 @@ export default function Groups() {
             iconWidget,
             nameWidget: <Label name={item.name[0].toUpperCase() + item.name.slice(1)} icon={iconWidget} className={item.disabled && styles.disabled} />,
             progress: !!hasStatusItem && <Progress variant={variant} tooltip={tooltip} size={48} style={{ flex: 0, justifyContent: "initial" }} value={variant === "determinate" ? statusItem.progress : undefined} />,
-            colorWidget: <ColorPicker pickerClassName={styles.picker} key={item.name} color={item.color} onChangeComplete={changeColor} />
+            colorWidget: <ColorPicker name={item.name} pickerClassName={styles.picker} key={item.name} color={item.color} onChangeComplete={changeColor} />
         };
     };
 
--- src/pages/Groups/ColorPicker.js
+++ src/pages/Groups/ColorPicker.js
@@ -1,26 +1,53 @@
 import React, { useState } from "react";
 import { SwatchesPicker } from "react-color";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
 import styles from "./ColorPicker.module.scss";
 import clsx from "clsx";
 
-export default function ColorPicker({ color, pickerClassName, onChangeComplete }) {
-    const [isVisible, show] = useState(false);
+export default function ColorPicker({ name, color, pickerClassName, onChangeComplete }) {
+    const [open, setOpen] = useState(false);
 
     const handleClick = () => {
-        show(true);
+        setOpen(true);
     };
 
     const handleClose = () => {
-        show(false);
+        setOpen(false);
     };
 
-    return (<>
-        <div className={styles.swatch} onClick={handleClick}>
-            <div className={styles.color} style={{ backgroundColor: color }} />
-        </div>
-        { isVisible && <div className={styles.popover}>
-            <div className={styles.cover} onClick={handleClose} />
-            <SwatchesPicker className={clsx(styles.picker, pickerClassName)} color={color} onChangeComplete={onChangeComplete} />
-        </div>}
-    </>);
+    return (
+        <>
+            <div className={styles.swatch} onClick={handleClick}>
+                <div className={styles.color} style={{ backgroundColor: color }} />
+            </div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                maxWidth="md"
+                fullWidth={false}
+                slotProps={{
+                    paper: {
+                        className: styles.dialogPaper
+                    }
+                }}
+            >
+                <div className={styles.pickerContainer}>
+                    <DialogTitle className={styles.dialogTitle}>
+                        {name[0].toUpperCase() + name.slice(1)}
+                    </DialogTitle>
+                    <SwatchesPicker
+                        className={clsx(styles.picker, pickerClassName)}
+                        color={color}
+                        width={500}
+                        height={380}
+                        onChangeComplete={(newColor) => {
+                            onChangeComplete(newColor);
+                            handleClose();
+                        }}
+                    />
+                </div>
+            </Dialog>
+        </>
+    );
 }
