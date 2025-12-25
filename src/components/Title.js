fbd18db04631284e44521c050246327b934afd57
zakaihamilton <zakaihamilton@gmail.com>
2025-12-25 23:31:58 +0200

    feat: conditionally hide root breadcrumb on mobile devices

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,10 @@
 import Breadcrumbs from "@components/Breadcrumbs";
 import { useActivePages } from "@util/pages";
 import styles from "./Title.module.scss";
+import { useDeviceType } from "@util/styles";
 
 export default function Title() {
     const pages = useActivePages();
-    return <Breadcrumbs className={styles.bar} items={pages} bar={true} border={true} hideRoot={true} />;
+    const isMobile = useDeviceType() !== "desktop";
+    return <Breadcrumbs className={styles.bar} items={pages} bar={true} border={true} hideRoot={isMobile} />;
 }
