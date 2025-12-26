e157d595c0d1026dc5b65a5907cef856c21ec411
zakaihamilton <zakaihamilton@gmail.com>
2025-12-26 15:25:11 +0200

    feat: add SessionsStore view mode to Title component's active pages.

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,12 @@ import { useActivePages } from "@util/pages";
 import styles from "./Title.module.scss";
 import { useDeviceType } from "@util/styles";
 import { ScheduleStore } from "@pages/Schedule";
+import { SessionsStore } from "@util/sessions";
 
 export default function Title() {
     const { viewMode } = ScheduleStore.useState();
-    const pages = useActivePages([viewMode]);
+    const { viewMode: sessionsViewMode } = SessionsStore.useState();
+    const pages = useActivePages([viewMode, sessionsViewMode]);
     const isMobile = useDeviceType() !== "desktop";
     return <Breadcrumbs className={styles.bar} items={pages} bar={true} border={true} hideRoot={isMobile} />;
 }
