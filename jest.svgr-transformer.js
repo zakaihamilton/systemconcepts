b61179ddb7d2fc4e95c3cc0b0f7e045bc13ff170
zakaihamilton <zakaihamilton@gmail.com>
2026-07-14 23:34:11 +0300

    Stabilize SVGR test caching and refresh service worker manifest

--- jest.svgr-transformer.js
+++ jest.svgr-transformer.js
@@ -1,11 +1,13 @@
 const { execFileSync } = require("node:child_process");
 const path = require("node:path");
 
+const compilerPath = path.join(__dirname, "jest.svgr-compile.cjs");
+
 module.exports = {
 	process(_sourceText, sourcePath) {
 		const code = execFileSync(
 			process.execPath,
-			[path.join(__dirname, "jest.svgr-compile.cjs"), sourcePath],
+			[compilerPath, sourcePath],
 			{ encoding: "utf8" },
 		);
 
